855f376a78ea3582cf0a16bd6aff3351cecfcf89
HubHop <zizhengpan98@gmail.com>
2021-08-23 20:39:18 +1000

    add scripts

--- scripts/train-tiny.sh
+++ scripts/train-tiny.sh
@@ -1,11 +1,36 @@
+#!/bin/bash
+
+#SBATCH --account=dl66
+#SBATCH --partition=dgx
+#SBATCH --qos=dgx
+
+#SBATCH -n 1
+#SBATCH -c 40
+#SBATCH --gres=gpu:V100:8
+#SBATCH --mem=400GB
+#SBATCH --time=24:00:00
+
+#SBATCH --mail-user=zizhengpan98@gmail.com
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+# SBATCH --exclude=dgx000
+
+
+# Command to run a gpu job
+# For example:
+# module load anaconda/2019.03-Python3.7-gcc5
+source activate quant
+which python
+
+nvidia-smi
 cd ../
-CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch \
-            --nproc_per_node=1 --master_port 1622 --use_env main.py  \
-            --model deit_ms_tiny_patch16_224  \
-            --batch-size 256 \
-            --data-path /home/datasets/cifar100  \
-            --data-set CIFAR \
-            --input-size 224  \
-            --output_dir ./exp_cuda/full-new-quant \
-            --num_workers 10 \
-            --ms_policy config/policy_tiny-8bit.txt 
+python -m torch.distributed.launch \
+        --nproc_per_node=8 --master_port 1622 --use_env main.py  \
+        --model deit_ms_tiny_patch16_224  \
+        --batch-size 128 \
+        --data-path /projects/dl65/m3_imagenet  \
+        --data-set CIFAR \
+        --input-size 224  \
+        --output_dir ./exp_cuda/full \
+        --num_workers 10 \
+        --ms_policy config/policy_tiny-8bit.txt 
